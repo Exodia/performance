34ad6275981d4cb979907e217f250bafb0661639
Exodia <d_xinxin@163.com>
2013-10-30 11:51:14 +0800

    update

--- performance.js
+++ performance.js
@@ -9,18 +9,42 @@ void function (win) {
     }
 
     win.__XPerformance = {
-        onload: function (record) {
+        record: {},
+        onload: function () {
             var start = new Date()
+            var record = this.record
             addEvent(win, 'load', function () {
                 record.loadTime = new Date() - start
             })
+
+            return this
         },
 
-        domReady: function (record) {
+        domReady: function () {
             var start = new Date()
+            var record = this.record
             addEvent(win.document, 'DOMContentLoaded', function () {
                 record.domReadyTime = new Date() - start
             })
+
+            return this
+        },
+
+        reporter: function () {
+            addEvent(win, 'load', function () {
+                var script = document.getElementById('__XPerformance')
+                if (!script) {
+                    script = document.createElement('script')
+                    var head = document.getElementsByTagName('head')[0]
+                    script.id = '__XPerformance'
+                    script.type = 'type/performance'
+                    head.insertBefore(script, head.firstChild)
+                }
+
+                script.innerHTML = JSON.stringify(this.record)
+            })
+
+            return this
         }
     }
 }(window);
